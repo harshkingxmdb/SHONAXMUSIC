fdec293519b9b80cd74e13e18c60e6f0f95ddad5
𝐒𝐇𝐎𝐍𝐀 𝐁𝐎𝐓𝐒🚩 <harshkumar12038@gmail.com>
2026-07-08 07:46:44 +0530

    Update autoplay.py

--- AloneX/plugins/autoplay.py
+++ AloneX/plugins/autoplay.py
@@ -3,10 +3,22 @@
 # This file is part of AloneXMusic
 
 
+import asyncio
+
 from pyrogram import filters, types
 
 from AloneX import app, db, lang
 from AloneX.helpers import can_manage_vc
+
+DELETE_DELAY = 7
+
+
+async def _delete_later(message: types.Message) -> None:
+    try:
+        await asyncio.sleep(DELETE_DELAY)
+        await message.delete()
+    except Exception:
+        pass
 
 
 @app.on_message(filters.command(["autoplay"]) & filters.group & ~app.bl_users)
@@ -18,28 +30,36 @@ async def _autoplay(_, m: types.Message):
         state = m.lang.get("autoplay_on", "Enabled") if status else m.lang.get(
             "autoplay_off", "Disabled"
         )
-        return await m.reply_text(
+        msg = await m.reply_text(
             m.lang.get("autoplay_status", "Autoplay is currently: {0}").format(state)
         )
+        asyncio.create_task(_delete_later(msg))
+        return
 
     mode = m.command[1].lower()
     if mode in ("on", "enable"):
         await db.set_autoplay(m.chat.id, True)
-        return await m.reply_text(
+        msg = await m.reply_text(
             m.lang.get(
                 "autoplay_enabled",
-                "🎶 Autoplay has been enabled.\n\nWhen the queue runs out, I'll automatically keep playing related songs.",
+                "🎶 Autoplay enabled.",
             )
         )
+        asyncio.create_task(_delete_later(msg))
+        return
     elif mode in ("off", "disable"):
         await db.set_autoplay(m.chat.id, False)
-        return await m.reply_text(
+        msg = await m.reply_text(
             m.lang.get(
                 "autoplay_disabled",
-                "🚫 Autoplay has been disabled.\n\nPlayback will stop once the queue is empty.",
+                "🚫 Autoplay disabled.",
             )
         )
+        asyncio.create_task(_delete_later(msg))
+        return
     else:
-        return await m.reply_text(
+        msg = await m.reply_text(
             m.lang.get("autoplay_usage", "Usage: /autoplay [on|off]")
         )
+        asyncio.create_task(_delete_later(msg))
+        return
